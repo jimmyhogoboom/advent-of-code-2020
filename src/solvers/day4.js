3799971ee26cb1f44857affaae8c0b7e466d33f9
Jimmy Hogoboom <jimmy@differential.com>
2020-12-04 10:28:51 -0600

    Day 4 part 2

--- src/solvers/day4.js
+++ src/solvers/day4.js
@@ -38,7 +38,58 @@ export function part1(lines) {
     .filter(Boolean).length;
 }
 
-export function part2(lines) {}
+export function year(min, max, val) {
+  const int = parseInt(val);
+  return int >= min && int <= max;
+}
+
+export function height(val) {
+  const type = val.slice(-2);
+  const value = parseInt(val.slice(0, 3));
+  if (type === 'cm') return value >= 150 && value <= 193;
+  if (type === 'in') return value >= 59 && value <= 76;
+
+  return false;
+}
+
+export function hairColor(val) {
+  return /#[\da-f]{6}/.test(val);
+}
+
+export function eyeColor(val) {
+  return ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'].includes(val);
+}
+
+export function pid(val) {
+  return /^\d{9}$/.test(val);
+}
+
+const testMap = {
+  hcl: hairColor,
+  iyr: (val) => year(2010, 2020, val),
+  eyr: (val) => year(2020, 2030, val),
+  byr: (val) => year(1920, 2002, val),
+  ecl: eyeColor,
+  pid: pid,
+  hgt: height,
+  cid: () => true,
+};
+
+function testPass(fields) {
+  return Object.entries(fields)
+    .map(([k, v]) => testMap[k](v))
+    .every(Boolean);
+}
+
+function test(pass) {
+  const required = ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid'];
+  const fields = getFields(pass);
+  return hasRequiredFields(required, fields) && testPass(fields);
+}
+
+export function part2(lines) {
+  return getPassports(lines).map(test).filter(Boolean).length;
+}
 
 export default function day4(lines) {
   return printParts(part1(lines), part2(lines));
--- src/solvers/day4.test.js
+++ src/solvers/day4.test.js
@@ -4,6 +4,11 @@ import {
   hasRequiredFields,
   part1,
   part2,
+  year,
+  height,
+  hairColor,
+  eyeColor,
+  pid,
 } from './day4';
 
 describe('day 4', () => {
@@ -128,5 +133,82 @@ describe('day 4', () => {
     ],
   ])('part 1', (pass, count) => {
     expect(part1(pass)).toBe(count);
+  });
+
+  test.each([
+    ['2002', true],
+    ['2003', false],
+  ])('year', (val, result) => {
+    expect(year(1920, 2002, val)).toBe(result);
+  });
+
+  test.each([
+    ['60in', true],
+    ['190cm', true],
+    ['190in', false],
+    ['190', false],
+  ])('height', (val, result) => {
+    expect(height(val)).toBe(result);
+  });
+
+  test.each([
+    ['#123abc', true],
+    ['#123abz', false],
+    ['123abc', false],
+  ])('hair color', (val, result) => {
+    expect(hairColor(val)).toBe(result);
+  });
+
+  test.each([
+    ['brn', true],
+    ['wat', false],
+  ])('eye color', (val, result) => {
+    expect(eyeColor(val)).toBe(result);
+  });
+
+  test.each([
+    ['000000001', true],
+    ['0123456789', false],
+  ])('pid', (val, result) => {
+    expect(pid(val)).toBe(result);
+  });
+
+  test('part 2 invalid', () => {
+    const lines = [
+      'eyr:1972 cid:100',
+      'hcl:#18171d ecl:amb hgt:170 pid:186cm iyr:2018 byr:1926',
+      '',
+      'iyr:2019',
+      'hcl:#602927 eyr:1967 hgt:170cm',
+      'ecl:grn pid:012533040 byr:1946',
+      '',
+      'hcl:dab227 iyr:2012',
+      'ecl:brn hgt:182cm pid:021572410 eyr:2020 byr:1992 cid:277',
+      '',
+      'hgt:59cm ecl:zzz',
+      'eyr:2038 hcl:74454a iyr:2023',
+      'pid:3556412378 byr:2007',
+    ];
+
+    expect(part2(lines)).toBe(0);
+  });
+
+  test('part 2 valid', () => {
+    const lines = [
+      'pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980',
+      'hcl:#623a2f',
+      '',
+      'eyr:2029 ecl:blu cid:129 byr:1989',
+      'iyr:2014 pid:896056539 hcl:#a97842 hgt:165cm',
+      '',
+      'hcl:#888785',
+      'hgt:164cm byr:2001 iyr:2015 cid:88',
+      'pid:545766238 ecl:hzl',
+      'eyr:2022',
+      '',
+      'iyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719',
+    ];
+
+    expect(part2(lines)).toBe(4);
   });
 });
