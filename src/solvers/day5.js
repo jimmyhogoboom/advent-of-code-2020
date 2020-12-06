ccc1b4e906e75bf31d14fcf602a01799aa88c6a4
Jimmy Hogoboom <jimmy@differential.com>
2020-12-05 23:44:57 -0600

    Day5 part 1 tests passing

---
+++ src/solvers/day5.js
@@ -0,0 +1,45 @@
+import printParts from '../util/printParts';
+
+const maxRows = 128;
+const maxColumns = 8;
+const lower = ['F', 'L'];
+const upper = ['B', 'R'];
+
+export function splitRow(min, max, dir) {
+  const range = max - min;
+
+  const newMin = lower.includes(dir) ? min : Math.ceil(range / 2) + min;
+
+  const newMax = upper.includes(dir) ? max : Math.floor(range / 2) + min;
+
+  return newMin === newMax ? newMin : [newMin, newMax];
+}
+
+export function getSeatId(row, column) {
+  return row * maxColumns + column;
+}
+
+export function getSeat(code) {
+  function getPosition(codePart, [min, max]) {
+    const newPos = splitRow(min, max, codePart[0]);
+    if (!Array.isArray(newPos) || codePart.length < 1) return newPos;
+    return getPosition(codePart.slice(1, codePart.length), newPos);
+  }
+
+  const matches = code.match(/([BF]*)([LR]+)/);
+  const rows = matches[1];
+  const columns = matches[2];
+
+  const row = getPosition(rows, [0, maxRows - 1]);
+  const column = getPosition(columns, [0, maxColumns - 1]);
+
+  return [row, column];
+}
+
+export function part1() {}
+
+export function part2() {}
+
+export default function day5(lines) {
+  return printParts(part1(lines), part2(lines));
+}
---
+++ src/solvers/day5.test.js
@@ -0,0 +1,41 @@
+import { splitRow, getSeatId, getSeat } from './day5';
+
+describe('day 5', () => {
+  describe('splitRow', () => {
+    test.each([
+      [0, 127, 'F', [0, 63]],
+      [0, 63, 'B', [32, 63]],
+      [32, 63, 'F', [32, 47]],
+      [32, 47, 'B', [40, 47]],
+      [40, 47, 'B', [44, 47]],
+      [44, 47, 'F', [44, 45]],
+      [44, 45, 'F', 44],
+      [0, 7, 'R', [4, 7]],
+      [4, 7, 'L', [4, 5]],
+      [4, 5, 'R', 5],
+    ])('splitRow(%s, %s, %s) = %s', (min, max, dir, result) => {
+      expect(splitRow(min, max, dir)).toStrictEqual(result);
+    });
+  });
+
+  describe('seatId', () => {
+    test.each([
+      [44, 5, 357],
+      [70, 7, 567],
+      [14, 7, 119],
+      [102, 4, 820],
+    ])('getSeatId', (row, column, id) => {
+      expect(getSeatId(row, column)).toBe(id);
+    });
+  });
+
+  describe('getSeat', () => {
+    test.each([
+      ['BFFFBBFRRR', [70, 7]],
+      ['FFFBBBFRRR', [14, 7]],
+      ['BBFFBBFRLL', [102, 4]],
+    ])('getSeat', (code, position) => {
+      expect(getSeat(code)).toStrictEqual(position);
+    });
+  });
+});
